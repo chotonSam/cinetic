a597992ee305d70fc9deb0504fb4485786d53ac7
chotonSam <fzsayemofficial@gmail.com>
2025-01-19 00:23:32 +0600

    changes

--- services/mongo.js
+++ services/mongo.js
@@ -8,8 +8,6 @@ export const dbConnect = async () => {
 
   try {
     await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
       serverSelectionTimeoutMS: 60000, // Timeout increased to 60 seconds
       connectTimeoutMS: 60000,
       socketTimeoutMS: 60000,
