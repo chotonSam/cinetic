66b3c3da00822e9aa8eebf2c60752f802d59cca0
chotonSam <fzsayemofficial@gmail.com>
2025-01-20 04:12:25 +0600

    almost final

--- components/menu/Logo.jsx
+++ components/menu/Logo.jsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 export default function Logo() {
   return (
-    <Link href="/" className="text-red-600  text-4xl font-bold">
+    <Link href="/" className="text-red-600 md:text-4xl text-3xl font-bold">
       CINETIC
     </Link>
   );
